146a7fe543835fd8ca5ff99dbbf55c4d107935c3
Dasha-Kinsely <dashakinsely@gmail.com>
2020-02-07 14:18:03 -0700

    fixed yesterday's errors

--- httpclient.py
+++ httpclient.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 # coding: utf-8
 # Copyright 2016 Abram Hindle, https://github.com/tywtyw2002, and https://github.com/treedust
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -22,18 +22,22 @@ import sys
 import socket
 import re
 # you may use urllib to encode data appropriately
-import urllib.parse
+from urllib.parse import urlparse as parser
+from urllib.parse import urlencode as encoder
+
 
 def help():
     print("httpclient.py [GET/POST] [URL]\n")
+
 
 class HTTPResponse(object):
     def __init__(self, code=200, body=""):
         self.code = code
         self.body = body
 
+
 class HTTPClient(object):
-    #def get_host_port(self,url):
+    # def get_host_port(self,url):
 
     def connect(self, host, port):
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -41,17 +45,20 @@ class HTTPClient(object):
         return None
 
     def get_code(self, data):
-        return None
+        # print(data.splitlines()[0].split()[1])
+        # get the code that's always in the first line of feedback
+        return int(data.splitlines()[0].split()[1])
 
-    def get_headers(self,data):
+    def get_headers(self, data):
         return None
 
     def get_body(self, data):
-        return None
-    
+        print(data.splitlines())
+        return data.split("\r\n\r\n")[1]
+
     def sendall(self, data):
         self.socket.sendall(data.encode('utf-8'))
-        
+
     def close(self):
         self.socket.close()
 
@@ -67,22 +74,70 @@ class HTTPClient(object):
                 done = not part
         return buffer.decode('utf-8')
 
+    def master_servicer(self, host, port, header):
+        self.connect(host, port)
+        self.sendall(header)
+        res = self.recvall(self.socket)
+        code = self.get_code(res)
+        body = self.get_body(res)
+        return code, body
+
+    def master_parser(self, callback, method, arg=None):
+        if callback.port == None:
+            port = 80
+        else:
+            port = callback.port
+        cquery, cparams, cfragment, cscheme, cpath, cnetloc, cpath = callback.query, callback.params, callback.fragment, callback.scheme, callback.path, callback.hostname, callback.path
+        if not cpath:
+            cpath = '/'
+        header = self.generate_header(method, cnetloc, cpath, arg)
+        return header, cnetloc, port
+
+    def generate_header(self, method, netloc, path, arg=None):
+        if method == 'GET':
+            return (method + ' ' + path + ' HTTP/1.1\r\n'
+                    'HOST: ' + netloc + '\r\n'
+                    'Connection: close\r\n'
+                    '\r\n')
+        elif method == 'POST':
+            if arg == None:
+                content_length = 0
+            else:
+                content_length = len(arg)
+            return (method + ' ' + path + ' HTTP/1.1\r\n' + 'HOST: ' + netloc + '\r\n'
+                    'Content-Type: application/x-www-form-urlencoded\r\n'
+                    'Content-Length: '+str(content_length)+'\r\n'
+                    'Connection: close\r\n\r\n' +
+                    str(arg))
+
     def GET(self, url, args=None):
         code = 500
         body = ""
+        callback = parser(url)
+        header, cnetloc, port = self.master_parser(callback, 'GET')
+        code, body = self.master_servicer(cnetloc, port, header)
+        #print('our result: ' + str(code)+str(body))
+        self.close()
         return HTTPResponse(code, body)
 
     def POST(self, url, args=None):
         code = 500
         body = ""
+        if args != None:
+            args = encoder(args)
+        header, cnetloc, port = self.master_parser(parser(url), 'POST', args)
+        code, body = self.master_servicer(cnetloc, port, header)
+        #print('our result: ' + str(code)+str(body))
+        self.close()
         return HTTPResponse(code, body)
 
     def command(self, url, command="GET", args=None):
         if (command == "POST"):
-            return self.POST( url, args )
-        else:
-            return self.GET( url, args )
-    
+            return self.POST(url, args)
+        elif (command == "GET"):
+            return self.GET(url, args)
+
+
 if __name__ == "__main__":
     client = HTTPClient()
     command = "GET"
@@ -90,6 +145,6 @@ if __name__ == "__main__":
         help()
         sys.exit(1)
     elif (len(sys.argv) == 3):
-        print(client.command( sys.argv[2], sys.argv[1] ))
+        print(client.command(sys.argv[2], sys.argv[1]))
     else:
-        print(client.command( sys.argv[1] ))
+        print(client.command(sys.argv[1]))
